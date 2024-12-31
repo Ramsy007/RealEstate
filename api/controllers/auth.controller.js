bf7e5b8f3e17d8a891c7db7567fea53c303fa0e9
Girdhar Mishra <girdharmishra10102003@gmail.com>
2024-12-31 16:45:20 +0530

    created signin page and functionality

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,8 @@
 
  import User from "../models/user.model.js"
  import bcrypt from 'bcrypt'
+import { errorhandler } from "../utills/error.js";
+import jwt from "jsonwebtoken";
  export const  signup= async(req,res,next)=>{
       const {userName,email,password}=req.body;
        const hashedpassword=await bcrypt.hash(password,10);
@@ -12,4 +14,26 @@
           next(err);
        }
        
+ }
+ export const signin=async(req,res,next)=>{
+  try{ const {email,password}=req.body;
+    const user=await User.findOne({email});
+     if(!user){
+        return res(next(errorhandler(401,"invalid credentials")));
+     }
+      const validpassword=await bcrypt.compare(password,user.password);
+      if(!validpassword)return next(errorhandler(401,"invalid credentials"));
+      const token=jwt.sign({_id:user._id},process.env.secret,{
+         expiresIn: "7d",
+       });
+       res.cookie('token',token,{httponly:true},{
+         expires: new Date(Date.now() + 8 * 3600000),
+       })
+       .status(200)
+       .json({user});}
+       catch(err){
+          next(err);
+       }
+
+
  }
